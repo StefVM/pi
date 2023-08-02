bbed0b7f4c09dc93f087961ce61b1796b3ecce14
StefVM <r0837625@student.thomasmore.be>
2023-08-02 22:55:47 +0200

    all but screen done

--- project.py
+++ project.py
@@ -1,10 +1,29 @@
 import RPi.GPIO as GPIO
 import time
+import cgitb ; cgitb.enable()
+import spidev
+import busio
+import digitalio
+import board
+from adafruit_bus_device.spi_device import SPIDevice
 
 try:
-    GPIO.setmode(GPIO.BOARD)
+    ControlPin = [6, 16, 15, 26]
 
-    ControlPin = [31, 36, 22, 37]
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+    cs0 = digitalio.DigitalInOut(board.CE0)
+    adc = SPIDevice(spi, cs0, baudrate= 1000000)
+
+    def readadc(adcnum):
+        if ((adcnum > 7) or (adcnum < 0)):
+            return -1
+        with adc:
+            r = bytearray(3)
+            spi.write_readinto([1, (8+adcnum)<<4, 0], r)
+            time.sleep(0.000005)
+            adcout = ((r[1]&3) << 8) + r[2]
+            return adcout
 
     for pin in ControlPin:
         GPIO.setup(pin, GPIO.OUT)
@@ -20,12 +39,11 @@ try:
         [0, 1, 1, 0],
         [1, 1, 0, 0]]
 
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(15, GPIO.IN)
-    GPIO.setup(29, GPIO.IN)
+    GPIO.setup(22, GPIO.IN)
+    GPIO.setup(5, GPIO.IN)
 
-    PIN_TRIGGER = 11
-    PIN_ECHO = 13
+    PIN_TRIGGER = 17
+    PIN_ECHO = 27
 
     GPIO.setup(PIN_TRIGGER, GPIO.OUT)
     GPIO.setup(PIN_ECHO, GPIO.IN)
@@ -39,6 +57,9 @@ try:
     print("Calculating distance")
 
     while True:
+        tmp = readadc(0)
+        print("input: ", tmp)
+        time.sleep(0.2)
         GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
         time.sleep(0.00001)
@@ -55,7 +76,8 @@ try:
         pulse_duration = pulse_end_time - pulse_start_time
         distance = round(pulse_duration * 17000, 2)
         print("Distance:",distance,"cm")
-        trigger_distance = 5
+        trigger_distance = 2 + tmp/100
+        print("Trigger distance:",trigger_distance,"cm")
         if (distance < trigger_distance):
             for i in range(0, 500):
                 for fullstep in range(4):
@@ -64,7 +86,7 @@ try:
                         time.sleep(0.001)
                         GPIO.cleanup
         
-        if (GPIO.input(15)==0):
+        if (GPIO.input(22)==0):
             print("click")
             for i in range(0, 500):
                 for fullstep in range(4):
@@ -74,7 +96,7 @@ try:
                         GPIO.cleanup
             time.sleep(0.3)
         
-        if (GPIO.input(29)==0):
+        if (GPIO.input(5)==0):
             print("click 2")
             for i in range(0, 500):
                 for fullstep in range(4):
