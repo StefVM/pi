85e8656e768f416f29bb7df7d6795a471585965e
StefVM <r0837625@student.thomasmore.be>
2023-08-02 23:20:42 +0200

    project works

--- project.py
+++ project.py
@@ -6,14 +6,33 @@ import busio
 import digitalio
 import board
 from adafruit_bus_device.spi_device import SPIDevice
+import adafruit_pcd8544
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 try:
-    ControlPin = [6, 16, 15, 26]
+    ControlPin = [6, 16, 25, 26]
 
     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
     cs0 = digitalio.DigitalInOut(board.CE0)
     adc = SPIDevice(spi, cs0, baudrate= 1000000)
+    dc = digitalio.DigitalInOut(board.D23)
+    csl = digitalio.DigitalInOut(board.CE1)
+    reset = digitalio.DigitalInOut(board.D24)
+    display = adafruit_pcd8544.PCD8544(spi, dc, csl, reset, baudrate=1000000)
+    display.bias = 4
+    display.contrast = 60
+    display.invert = True
+
+    display.fill(0)
+    display.show()
+
+    font = ImageFont.load_default()
+
+    image = Image.new('1', (display.width, display.height))
+    draw = ImageDraw.Draw(image)
 
     def readadc(adcnum):
         if ((adcnum > 7) or (adcnum < 0)):
@@ -38,6 +57,9 @@ try:
         [0, 0, 1, 1],
         [0, 1, 1, 0],
         [1, 1, 0, 0]]
+    
+    status = "on"
+    triggercount = 0
 
     GPIO.setup(22, GPIO.IN)
     GPIO.setup(5, GPIO.IN)
@@ -66,11 +88,15 @@ try:
 
         GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
+        print(GPIO.input(PIN_ECHO))
         while GPIO.input(PIN_ECHO)==0:
             pulse_start_time = time.time()
+        print(0)
         
+        print(GPIO.input(PIN_ECHO))
         while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()        
+            pulse_end_time = time.time()
+        print(1)
         
 
         pulse_duration = pulse_end_time - pulse_start_time
@@ -78,34 +104,59 @@ try:
         print("Distance:",distance,"cm")
         trigger_distance = 2 + tmp/100
         print("Trigger distance:",trigger_distance,"cm")
-        if (distance < trigger_distance):
-            for i in range(0, 500):
-                for fullstep in range(4):
-                    for pin in range(4):
-                        GPIO.output(ControlPin[pin], seq[fullstep] [pin])
-                        time.sleep(0.001)
-                        GPIO.cleanup
+        if status == "on":
+            if (distance < trigger_distance):
+                for i in range(0, 500):
+                    for fullstep in range(4):
+                        for pin in range(4):
+                            GPIO.output(ControlPin[pin], seq[fullstep] [pin])
+                            time.sleep(0.001)
+                            GPIO.cleanup
+                triggercount += 1
+                status = "triggered"
         
-        if (GPIO.input(22)==0):
-            print("click")
-            for i in range(0, 500):
-                for fullstep in range(4):
-                    for pin in range(4):
-                        GPIO.output(ControlPin[pin], seq[fullstep] [pin])
-                        time.sleep(0.001)
-                        GPIO.cleanup
-            time.sleep(0.3)
+        if status == "on":
+            if (GPIO.input(22)==0):
+                print("click")
+                for i in range(0, 500):
+                    for fullstep in range(4):
+                        for pin in range(4):
+                            GPIO.output(ControlPin[pin], seq[fullstep] [pin])
+                            time.sleep(0.001)
+                            GPIO.cleanup
+                triggercount += 1
+                status = "triggered"
+                time.sleep(0.3)
         
-        if (GPIO.input(5)==0):
-            print("click 2")
-            for i in range(0, 500):
-                for fullstep in range(4):
-                    for pin in range(4):
-                        GPIO.output(ControlPin[pin], reverseseq[fullstep] [pin])
-                        time.sleep(0.001)
-                        GPIO.cleanup
-            time.sleep(0.3)
+        if status == "triggered":
+            if (GPIO.input(5)==0):
+                print("click 2")
+                for i in range(0, 500):
+                    for fullstep in range(4):
+                        for pin in range(4):
+                            GPIO.output(ControlPin[pin], reverseseq[fullstep] [pin])
+                            time.sleep(0.001)
+                            GPIO.cleanup
+                status = "off"
+                draw.text((1,0), 'Dist: ' + str(distance) + "cm", font=font)
+                draw.text((1,8), 'Trig: ' + str(trigger_distance) + "cm", font=font)
+                draw.text((1,16), 'Status: ' + status, font=font)
+                draw.text((1,24), 'Triggers: ' + str(triggercount), font=font)
+                display.image(image)
+                display.show()
+                time.sleep(20)
+                status = "on"
+                time.sleep(0.3)
         
+        draw.rectangle((0, 0, display.width, display.height), outline=255, fill=255)
+
+        draw.text((1,0), 'Dist: ' + str(distance) + "cm", font=font)
+        draw.text((1,8), 'Trig: ' + str(trigger_distance) + "cm", font=font)
+        draw.text((1,16), 'Status: ' + status, font=font)
+        draw.text((1,24), 'Triggers: ' + str(triggercount), font=font)
+        display.image(image)
+        display.show()
+
         time.sleep(2)
     
 finally:
